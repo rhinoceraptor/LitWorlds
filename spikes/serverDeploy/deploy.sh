c80a04b7b62d0e56b5bb16b2b1e7b1525ce6c26b
jLewis <rhinoceraptor@gmail.com>
2014-04-08 19:36:52 -0400

    added useradd stuff

--- spikes/serverDeploy/deploy.sh
+++ spikes/serverDeploy/deploy.sh
@@ -1,18 +1,30 @@
-# Server deploy script
+#! /bin/bash
+#########################################################
+# Server deploy script from a bare debian install
 # LambdaMOO and enCore db
 # Literary Worlds - CS 4900
 # Jack Lewis
 #########################################################
-# Constants
+# URLs for server and db
 ENCORE_URL=http://downloads.sourceforge.net/project/ele/enCore%20Database/enCore%204.0.1/enCore-4.0.1.tar.gz
 LAMBDA_MOO_URL=http://lingo.uib.no/v5/downloads/LambdaMOO-1.8.1-unicode.tar.gz
 #########################################################
+
+# install sudo, apache-webserver, gcc
+apt-get install sudo apache2, gcc
+
+# create a moo user, with sudo group, 
+useradd -g wheel -G admin -s /bin/bash -p users -d /home/moo -m moo
+
+# make /usr/local/moo directory
+mkdir /usr/local/moo
+cd /usr/local/moo
 
 # wget both files
 wget ${ENCORE_URL}
 wget ${LAMBDA_MOO_URL}
 
-# untar LambdaMOO and enCore
+# untar LambdaMOO and enCore to /usr/local/moo
 tar -zxvf LambdaMOO-1.8.1-unicode.tar.gz
 tar -zxvf enCore-4.0.1.tar.gz
 
@@ -20,4 +32,13 @@ tar -zxvf enCore-4.0.1.tar.gz
 mkdir src
 mv *.tar.gz src
 
-# 
+# compile the MOO server
+cd MOO-1.8.1
+sh configure
+make
+
+# make bin directory, copy the moo server and the restart script to it
+cd ..
+mkdir bin
+cp MOO-1.8.1/moo bin/
+cp MOO-1.8.1/restart bin/
