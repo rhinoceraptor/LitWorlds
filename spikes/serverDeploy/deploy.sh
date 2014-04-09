e7ed39214c97420367ba8905244f0013dc2534eb
jLewis <rhinoceraptor@gmail.com>
2014-04-09 11:59:41 -0400

    almost working, need to make moo user run the server, need to disable root ssh, need to get Xpress to run right, then add stuff to configure wizards

--- spikes/serverDeploy/deploy.sh
+++ spikes/serverDeploy/deploy.sh
@@ -8,18 +8,21 @@
 # URLs for server and db
 ENCORE_URL=http://downloads.sourceforge.net/project/ele/enCore%20Database/enCore%204.0.1/enCore-4.0.1.tar.gz
 LAMBDA_MOO_URL=http://lingo.uib.no/v5/downloads/LambdaMOO-1.8.1-unicode.tar.gz
+# Directories for installation
 INSTALLDIR=/usr/local/moo
-APACHEDIR=/etc/apache2/apache2.conf
+APACHEDIR=/etc/apache2
 #########################################################
 
-# If anything goes wrong, abort the script
+# If anything goes wrong (command returns non-zero), abort the script
 set -e
 
-# install sudo, apache-webserver, gcc
-apt-get install sudo apache2 gcc make bison
+# install updates, sudo, apache, gcc, bison, vim non-interatively
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get -q -y install sudo apache2 gcc make bison vim csh
 
 # create a moo user, with sudo group, 
-useradd -g wheel -s /bin/bash -p users -d /home/moo -m moo
+useradd -g sudo -s /bin/bash -p users -d /home/moo -m moo
 
 # make INSTALLDIR directory
 mkdir ${INSTALLDIR}
@@ -51,12 +54,14 @@ cp MOO-1.8.1/restart bin/
 
 # go to the apache install directory and create an alias for Xpress
 cd ${APACHEDIR}
-echo -n "# Alias for encore" >> apache.conf
-echo -n "Alias /encore \"/usr/local/moo/encore\"" >> apache.conf
+echo -n "# Alias for encore\n" >> apache2.conf
+echo -n "Alias /encore \"/usr/local/moo/encore\"" >> apache2.conf
 
 # Add enCore.db, moo, and restart to the bin directory
-cd ${INSTALLDIR}
-
+cd ${INSTALLDIR}/bin
+cp ../MOO-1.8.1/moo .
+cp ../MOO-1.8.1/restart .
+cp ../encore/enCore.db .
 
 # check that the bin directory has the correct contents
 cd ${INSTALLDIR}/bin
@@ -69,3 +74,8 @@ else
 	echo -n "Incorrect Directory contents"
 	exit 1
 fi
+
+# Restart the enCore server
+cd ${INSTALLDIR}/bin
+chmod +x restart
+./restart enCore
