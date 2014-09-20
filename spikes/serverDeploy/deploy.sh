690128864a6fded2bd31f23f5867b64467b9515a
Jack Lewis <rhinoceraptor@gmail.com>
2014-09-20 11:16:58 -0400

    Update deploy.sh

--- spikes/serverDeploy/deploy.sh
+++ spikes/serverDeploy/deploy.sh
@@ -6,7 +6,8 @@
 # Jack Lewis
 #########################################################
 # URLs for server and db
-ENCORE_URL=http://downloads.sourceforge.net/project/ele/enCore%20Database/enCore%204.0.1/enCore-4.0.1.tar.gz
+#ENCORE_URL=http://downloads.sourceforge.net/project/ele/enCore%20Database/enCore%204.0.1/enCore-4.0.1.tar.gz
+ENCORE_URL=http://www.encore-consortium.org/Barn/files/docs/Ewebbed-enCore-V4-a3.zip
 LAMBDA_MOO_URL=http://lingo.uib.no/v5/downloads/LambdaMOO-1.8.1-unicode.tar.gz
 # Directories for installation
 INSTALLDIR=/usr/local/moo
@@ -33,7 +34,7 @@ wget ${ENCORE_URL}
 wget ${LAMBDA_MOO_URL}
 
 # untar LambdaMOO and enCore to ${INSTALLDIR}
-tar -zxvf LambdaMOO-1.8.1-unicode.tar.gz
+unzip Ewebbed-enCore-V4-a3
 tar -zxvf enCore-4.0.1.tar.gz
 
 # make src directory and move source files to src directory
